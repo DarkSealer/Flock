af131be94c2c9ddb10f84ff6d436d168f0b5207a
Leontin <leontin91andrei@gmail.com>
2019-10-17 14:59:59 +0300

    Evitarea obstacolelor a fost implementata

---
+++ Flock/Assets/Scripts/FilterScripts/PhysicsLayerFilter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Filter/Physics Layer")]
+public class PhysicsLayerFilter : ContextFilter
+{
+    public LayerMask mask;
+
+    public override List<Transform> Filter(FlockAgent agent, List<Transform> original)
+    {
+        List<Transform> filtered = new List<Transform>();
+        foreach (var item in original)
+        {
+            // daca obiectele sunt pe acelasi layer
+            if (mask == (mask | (1 << item.gameObject.layer)))
+            {
+                filtered.Add(item);
+            }
+        }
+        return filtered;
+    }
+}
