e8969a1cce99779f702d8d940ba93039cdd4fb7f
Leontin <leontin91andrei@gmail.com>
2019-10-17 09:05:28 +0300

    Flocking building started

---
+++ Flock/Assets/Scripts/Flock.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockAgent agentPrefab;
+    public FlockBehavior behavior;
+    [Range(10, 500)]
+    public int startingCount = 250;
+    [Range(1f, 100f)]
+    public float driveFactor = 10f;
+    [Range(1f, 100f)]
+    public float maxSpeed = 5f;
+    [Range(1f, 10f)]
+    public float neighborRadius = 1.5f;
+    [Range(0f, 1f)]
+    public float avoidanceRadiusMultiplier = 0.5f;
+
+
+    private List<FlockAgent> agents = new List<FlockAgent>();
+    private const float agentDensity = 0.08f;
+    private float squareMaxSpeed;
+    private float squareNeighborRadius;
+    private float squareAvoidanceRadius;
+
+    public float SquareAvoidanceRadius { get => squareAvoidanceRadius; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        squareMaxSpeed = maxSpeed * maxSpeed;
+        squareNeighborRadius = neighborRadius * neighborRadius;
+        squareAvoidanceRadius = squareNeighborRadius * avoidanceRadiusMultiplier * avoidanceRadiusMultiplier;
+
+        for (int i = 0; i < startingCount; i++)
+        {
+            // instantiaza un agent
+            FlockAgent newAgent = Instantiate(
+                agentPrefab,
+                Random.insideUnitCircle * startingCount * agentDensity,
+                Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)),
+                transform
+                );
+            newAgent.name = "Agent " + i;
+            agents.Add(newAgent);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
