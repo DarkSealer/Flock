746ede65f06311072314440df703c2c5cd9ea78b
Leontin <leontin91andrei@gmail.com>
2019-10-17 14:28:57 +0300

    Multiple stoluri lucreaza impreuna ca 1 ATENTIE! Se modifica in versiunea urmatoare

---
+++ Flock/Assets/Scripts/Behavior Scripts/PhysicsLayerFilter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Filter/Physics Layer")]
+public class PhysicsLayerFilter //: ContextFilter
+{
+
+}
